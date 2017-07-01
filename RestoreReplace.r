8e4ca2c8a084513ed26c51998b15fa6cc1df7cc1
Regina_Turing <ellenpain@163.com>
2017-07-01 11:59:50 +0800

    findreplace

--- RestoreReplace.r
+++ RestoreReplace.r
@@ -19,3 +19,33 @@ isNA<-subset(data,is.na(data[c(3)] == TRUE))
 isduty<-subset(data,is.na(data[c(3)] == FALSE))
 write.xlsx(isNA,"NAs.xlsx")
 write.xlsx(isduty,"isduty.xlsx")
+
+ref$empid<-as.character(ref$empid)
+empidlist<-as.character(isNA$empid)
+
+findreplace<-function(x){
+	ref$empid<-as.character(ref$empid)
+	replaceid<-c(NA)
+	num<-c(0)
+	replaceset<-data.frame(replaceid,num)
+	for(i in 1:158){
+		id<-x[i]
+		temp<-subset(ref,(ref$empid == id)==TRUE,select = c(replaceid,ratio))
+		if((is.data.frame(temp) && nrow(temp)==0)==FALSE)
+		{replaceid<-subset(temp,temp$ratio == max(ratio),select = replaceid)
+		replaceid<-data.frame(replaceid)
+		replaceid$num<-i
+		replaceset<-rbind(replaceset,replaceid)
+		}
+		else{
+		replaceid<-NA
+		replaceid<-data.frame(replaceid)
+		replaceid<-i
+		replaceset<-rbind(replaceset,replaceid)
+		}
+	}
+	return(replaceset)
+}
+
+Listofreplace<-findreplace(empidlist)
+write.xlsx(Listofreplace,"Result")
